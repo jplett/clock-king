308f1adedbeab012491e35dab27cd241e4097784
dartigula <jamesplett@outlook.com>
2015-09-09 23:53:44 +0000

    Update first.sh

--- first.sh
+++ first.sh
@@ -1,24 +1,10 @@
-# This will install ruby, greenletters, then run clock.sh
-
 PKG_OK=$(dpkg-query -W --showformat='${Status}\n' ruby|grep "installed")
-#echo "Checking for ruby."
 if [ "$PKG_OK" != "install ok installed" ] ; then
 	echo "No ruby. Setting up ruby."
 	apt-get --force-yes --yes install ruby
 	gem install greenletters
 	echo ""
 fi
-#echo "Ruby installed."
-
-#dogs will print a number of blank lines less than cats
-
-#dogs=0
-#cats=2
-#while [ $dogs -lt $cats ]
-#do
-#	echo ""
-#	dogs=$(( dogs+1 ))
-#done
 
 ## this is the clock code.
 
@@ -30,5 +16,6 @@ do
 	echo ""
 	~/configure-clock.rb $ip_addy
 	echo ""
-	read -p "Try again with the same IP? [Y/N] " y
+	read -p "Try again with $ip_addy? [Y/N] " y
+	echo ""
 done
