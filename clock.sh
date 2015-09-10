6cf67ed391f0901cd840ecedd4d0e8b627e8527d
dartigula <jamesplett@outlook.com>
2015-09-09 17:06:57 -0700

    Delete clock.sh

--- clock.sh
+++
@@ -1,8 +0,0 @@
-chmod +x ~/configure-clock.rb
-read -p "Enter IP Address: " ip_addy
-y="y"
-while [ $y != "N" ]
-do
-	~/configure-clock.rb $ip_addy
-	read -p "Try again with the same IP? [Y/N] " y
-done
